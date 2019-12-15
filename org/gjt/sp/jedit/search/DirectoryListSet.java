d52e80888340385149c456110e4f0d16b1c8dca8
funatake <centos@localhost.localdomain>
2019-12-15 10:40:05 +0900

    ローカルでgrepできるようになった

--- org/gjt/sp/jedit/search/DirectoryListSet.java
+++ org/gjt/sp/jedit/search/DirectoryListSet.java
@@ -28,6 +28,8 @@ import java.io.*;
 import org.gjt.sp.jedit.io.*;
 import org.gjt.sp.jedit.*;
 import org.gjt.sp.util.StandardUtilities;
+import java.util.List;
+import java.util.ArrayList;
 //}}}
 
 /**
@@ -108,13 +110,29 @@ public class DirectoryListSet extends BufferListSet
 	@Override
 	protected String[] _getFiles(final Component comp)
 	{
-		boolean skipBinary, skipHidden;
+		boolean skipBinary, skipHidden, useGrep;
 		skipBinary = jEdit.getBooleanProperty("search.skipBinary.toggle");
 		skipHidden = jEdit.getBooleanProperty("search.skipHidden.toggle");
+		useGrep = jEdit.getBooleanProperty("search.useGrep.toggle");
+		
+		if (useGrep 
+			&& (!MiscUtilities.isURL(directory) 
+				|| "sftp".equals(MiscUtilities.getProtocolOfURL(directory))
+				)
+			)
+		{
+			return _getFilesUseGrep(comp, skipBinary, skipHidden, useGrep);
+		} else {
+			return _getFilesUseVFS(comp, skipBinary, skipHidden);
+		}
+		
+	}
+	
+	protected String[] _getFilesUseVFS(final Component comp, boolean skipBinary, boolean skipHidden) {
 		final VFS vfs = VFSManager.getVFSForPath(directory);
 		Object session;
 		session = vfs.createVFSSessionSafe(directory, comp);
-
+		
 		try
 		{
 			try
@@ -132,7 +150,122 @@ public class DirectoryListSet extends BufferListSet
 				{ io.toString() });
 			return null;
 		}
-	} //}}}
+	}
+	
+	protected String[] _getFilesUseGrep(final Component comp, boolean skipBinary, boolean skipHidden, boolean useGrep) {
+		// macで regextype オプションが使えないため、grepでファイルを絞り込み
+		// find "./te st" -type f -regextype posix-egrep  -iregex ".?(/[^.][^/] *)*(javA|text)" -print0 | xargs -0 grep -l  -i -E $'TEST'
+		// 以下のコマンドを実行する
+		// find "." -type f | grep -i -E "^.?(/[^.][^/]*)*$" | grep -i -E ".*(java|text)" | sed -e 's/ /\\ /g' | xargs grep -l  -i -E $'hoge4'
+		StringBuilder sb = new StringBuilder();
+		sb.append("find ");
+		sb.append("\"").append(directory).append("\" ");
+		sb.append("-type f ");
+		if (!recurse) {
+			sb.append("-maxdepth 1 ");
+		} 
+		
+		if (skipHidden) {
+			sb.append(" | grep -E \"^(/[^.][^/]*)*$\"");
+		}
+		
+		sb.append(" | grep -i -E \"").append(StandardUtilities.globToRE(glob)).append("$\" ");
+		sb.append(" | sed -e 's/ /\\\\ /g' ");
+		
+		sb.append(" | xargs grep -l ");
+		if (SearchAndReplace.getIgnoreCase()) {
+			sb.append("-i ");
+		}
+		if (SearchAndReplace.getWholeWord()) {
+			sb.append("-w ");
+		}
+		if (skipBinary) {
+			sb.append("-I ");
+		}
+		
+		String searchString = SearchAndReplace.getSearchString();
+		if (SearchAndReplace.getRegexp()) {
+			// searchString = searchString.replaceAll("\n", "\\\\n");
+			// searchString = searchString.replaceAll("\\\\\\\\", "\\\\\\\\\\\\\\\\");
+			// searchString = searchString.replaceAll("'", "\\\\'");
+			// sb.append("-E $'").append(searchString).append("' ");
+			
+			searchString = searchString.replaceAll("\\\\\\\\", "\\\\\\\\\\\\\\\\");
+			searchString = searchString.replaceAll("'", "\\\\'");
+			sb.append("-P $'").append(searchString).append("' ");
+		} else {
+			searchString = searchString.replaceAll("\"", "\\\\\"");
+			sb.append("-F \"").append(searchString).append("\" ");
+		}
+		
+		sb.append("| sort ");
+		System.out.println(sb.toString());
+		
+		ArrayList<String> commands = new ArrayList();
+		commands.add("/bin/sh");
+		commands.add("-c");
+		commands.add(sb.toString());
+		
+		try {
+			String result = exec(commands, "UTF-8", "UTF-8");
+			if ("".equals(result)) {
+				return null;
+			} 
+			return result.split("\n");
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		
+		return null;
+	}
+	
+	public static String exec(List<String> command, String outEncoding, String inEncoding) throws IOException {
+		String lineSep = "\n";
+		BufferedReader pbr = null;
+		BufferedReader pbe = null;
+		StringBuffer result = new StringBuffer();
+		
+		try {
+			String[] commandArray = new String[command.size()];
+			command.toArray(commandArray);
+			
+			Runtime runtime = Runtime.getRuntime();
+			Process p = runtime.exec(commandArray);
+			
+			pbr = new BufferedReader(new InputStreamReader(p.getInputStream(), inEncoding));
+			pbe = new BufferedReader(new InputStreamReader(p.getErrorStream(), inEncoding));
+			
+			String line = null;
+			while ( (line = pbr.readLine()) != null){
+				result.append(line);
+				result.append(lineSep);
+			}
+			pbr.close();
+			
+			while ( (line = pbe.readLine()) != null){
+				System.err.println(line);
+			}
+			pbe.close();
+			
+		} finally {
+			close(pbr);
+			close(pbe);
+		}
+		
+		return result.toString();
+	}
+  
+  public static void close(Closeable closeable) {
+  	  try {
+  	  	  if (closeable != null) {
+  	  	  	  closeable.close();
+  	  	  }
+  	  } catch (Exception e) {
+  	  }
+  }
+	
+	//}}}
 
 	//{{{ Private members
 	private String directory;
